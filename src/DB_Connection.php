bfd1cecfb5e0d56172147d7659d435959915363a
Mostafa El-mezain <51489054+mostafa-elmezain@users.noreply.github.com>
2023-12-05 03:30:43 +0200

    Update DB_Connection.php

--- src/DB_Connection.php
+++ src/DB_Connection.php
@@ -5,7 +5,7 @@
         define('DB_HOST','localhost');
         define('DB_USER','root');
         define('DB_PASSWORD','');
-        define('DB_DATABASE','z_test_scandiweb');
+        define('DB_DATABASE','scandiweb');
 
         class DB_Connection{
 
